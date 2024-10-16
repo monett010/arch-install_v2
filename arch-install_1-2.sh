172532ff18158fd714c1ab24dabc305e471a202d
monett010 <acts2018+gh@gmail.com>
2024-10-16 12:58:11 -0400

    Added question and case statement for what desktop environment will be installed.

--- arch-install_1-2.sh
+++ arch-install_1-2.sh
@@ -3,17 +3,52 @@
 source desktop_env.sh
 
 # installing some important programs first...
-echo 'Installing some important programs first...'
+echo "Installing some important programs first..."
 pacman -S openssh tmux
 systemctl enable sshd.service
 
+echo "Installing video drivers..."
 installVideoDrivers
-installDisplayManager kde
-installDesktopEnv kde
+
+read -p "What desktop environment will you be installing?
+[1] budgie
+[2] cinnamon
+[3] gnome
+[4] kde " desktop_env
+
+case $desktop_env in
+    "1")
+        installDisplayManager budgie
+        installDesktopEnv budgie
+        installGTKThemes
+        ;;
+    "2")
+        installDisplayManager cinnamon
+        installDesktopEnv cinnamon
+        installGTKThemes
+        ;;
+    "3")
+        installDisplayManager gnome
+        installDesktopEnv gnome
+        installGTKThemes
+        ;;
+    "4")
+        installDisplayManager kde
+        installDesktopEnv kde
+        ;;
+esac
+
+# installDisplayManager kde
+# installDesktopEnv kde
 # installGTKThemes
+
+echo "Installing other apps..."
 installOtherApps
 
+echo "Configuring Xorg..."
 configureXorg
+
+echo "Setting locale and hostname..."
 setLocale
 setHostname test-arch
 
