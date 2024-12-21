833d18e544e5773676f84105aba3ea438e387257
monett010 <acts2018+gh@gmail.com>
2024-12-20 20:22:45 -0500

    Coded 3 and 3_2

---
+++ arch-install_3-2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Creating /boot/efi directory...'
+mkdir /boot/efi
+
+read -p "What is the /boot/efi partition?" $boot
+
+echo 'Mounting /boot/efi partition...'
+mount "$boot" /boot/efi
+
+# setting up grub
+echo 'Setting up GRUB...'
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
---
+++ arch-install_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+read -p "What partition does / go?" $root_ 
+
+echo 'Unmounting partitions...'
+umount /mnt
+
+umount /mnt/boot/efi
+
+echo 'Remounting root partition...'
+mount "$root_" /mnt
+
+arch-chroot /mnt
+
