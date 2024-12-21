0047725827e0af605a43e1016bda72f58bb2ad99
monett010 <acts2018+gh@gmail.com>
2024-12-21 11:09:57 -0500

    Added plasma-nm package to kde install for network manager gui

--- desktop_env.sh
+++ desktop_env.sh
@@ -55,7 +55,7 @@ function installDesktopEnv(){
 		install_="cinnamon mate-terminal pluma kupfer plank"
 		;;
 	'kde')
-		install_="plasma-desktop kde-utilities-meta kde-system-meta"
+		install_="plasma-desktop kde-utilities-meta kde-system-meta plasma-nm"
 		;;
 	'gnome')
 		install_="gnome"
