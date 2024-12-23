6d6f3461c901af9dee0caff0eaa196f83506a1ea
monett010 <acts2018+gh@gmail.com>
2024-12-23 16:07:20 +0000

    Added nodejs to apps install

--- desktop_env.sh
+++ desktop_env.sh
@@ -73,7 +73,7 @@ function installGTKThemes(){
 }
 
 function installOtherApps(){
-	install_="firefox flatpak neofetch neovim zsh python-virtualenv python-pip"
+	install_="firefox flatpak neofetch neovim zsh python-virtualenv python-pip nodejs"
 	pacman -S $install_ $1
 }
 
