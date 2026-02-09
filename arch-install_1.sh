10058a5545f702451d6d05209777e809b89827e6
monett010 <acts2018+gh@gmail.com>
2026-02-09 13:00:40 -0500

    Added ability to choose which kernel to install. Added dkms to initial installs.

--- arch-install_1.sh
+++ arch-install_1.sh
@@ -12,8 +12,28 @@ mount "$bootloader_" /mnt/boot
 mount "$root" /mnt
 
 # installing linux kernel, headers, firmware, and base packages
-echo 'Installing linux kernel, headers, firmware, and base packages...'
-pacstrap -K /mnt linux linux-headers linux-firmware base base-devel nano vim pacman-contrib
+# echo 'Installing linux kernel, headers, firmware, and base packages...'
+# pacstrap -K /mnt linux linux-headers linux-firmware base base-devel nano vim pacman-contrib
+
+read -p "Which kernel would you like to install?
+[1] linux (default)
+[2] linux lts " kernel_
+
+case $kernel_ in
+    "1")
+        echo 'Installing linux kernel, headers, firmware, and base packages...'
+        pacstrap -K /mnt linux linux-headers linux-firmware base base-devel dkms nano vim pacman-contrib
+        ;;
+    "2")
+        echo 'Installing linux-lts kernel, headers, firmware, and base packages...'
+        pacstrap -K /mnt linux-lts linux-lts-headers linux-firmware base base-devel dkms nano vim pacman-contrib
+        ;;
+    *)
+        echo 'Installing linux kernel, headers, firmware, and base packages...'
+        pacstrap -K /mnt linux linux-headers linux-firmware base base-devel dkms nano vim pacman-contrib
+        ;;
+esac
+
 
 cp -r ~/arch-install_v2 /mnt
 
