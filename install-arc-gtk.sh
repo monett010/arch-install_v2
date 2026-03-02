96e479bdfe34a02bf9b734c29b8af62fa89b12e8
monett010 <acts2018+gh@gmail.com>
2026-03-02 16:54:46 -0500

    Changed it so ark-gtk-theme gets cloned to home folder

--- install-arc-gtk.sh
+++ install-arc-gtk.sh
@@ -1,7 +1,7 @@
 function installArcGTKTheme(){
 	echo "Installing Arc GTK theme..."
-	git clone https://aur.archlinux.org/arc-gtk-theme.git /arc-gtk-theme
-	cd /arc-gtk-theme
+	git clone https://aur.archlinux.org/arc-gtk-theme.git ~/arc-gtk-theme
+	cd ~/arc-gtk-theme
 	makepkg -is --skippgpcheck
 }
 
