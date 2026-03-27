d579fef46643708a7b3f73086b9f8f483ba897f0
monett010 <acts2018+gh@gmail.com>
2026-03-27 16:02:47 -0400

    Added os-prober to grub install

--- arch-install_3-2.sh
+++ arch-install_3-2.sh
@@ -14,7 +14,7 @@ mount "$boot_" /boot/efi
 
 # setting up grub
 echo 'Setting up GRUB...'
-pacman -S grub efibootmgr
+pacman -S grub efibootmgr os-prober
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
