15b6df6c2913e5ad7f61aa390e2a9a1572f3f64a
monett010 <acts2018+gh@gmail.com>
2026-03-27 15:16:15 -0400

    no preceding slashes

--- arch-install_4.sh
+++ arch-install_4.sh
@@ -6,7 +6,7 @@ function mountExtraPartitions () {
     read -p "Is there another partition you'd like to mount before generating fstab? y/n (default): " mount_extra
     if [ "$mount_extra" == "y" ]; then
         read -p "Please type the partition you'd like to mount: " partition
-        read -p "Please type where you'd like to mount it: " location
+        read -p "Please type where you'd like to mount it (no preceding slashes): " location
         mount --mkdir "$partition" /mnt/"$location"
 
         read -p "Would you like to add another partition? y/n (default)" mount_another
