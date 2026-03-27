97d4f37fbefe490b36770fe78d43f5a44851d69a
monett010 <acts2018+gh@gmail.com>
2026-03-27 15:14:16 -0400

    Added mountExtraPartitions function to arch-install_4

--- arch-install_4.sh
+++ arch-install_4.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+# Ask if there's another disk you'd like to mount
+
+function mountExtraPartitions () {
+    read -p "Is there another partition you'd like to mount before generating fstab? y/n (default): " mount_extra
+    if [ "$mount_extra" == "y" ]; then
+        read -p "Please type the partition you'd like to mount: " partition
+        read -p "Please type where you'd like to mount it: " location
+        mount --mkdir "$partition" /mnt/"$location"
+
+        read -p "Would you like to add another partition? y/n (default)" mount_another
+        if [ "$mount_another" == "y" ]; then
+            mountExtraPartitions
+        fi
+    fi
+}
+mountExtraPartitions
 # generate fstab
+echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
