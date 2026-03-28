d13fc8219c2fc05cfb06266cfec0cca25bc53a61
monett010 <acts2018+gh@gmail.com>
2026-03-27 22:31:24 -0400

    Added comments to install-arc-gtk.sh

--- install-arc-gtk.sh
+++ install-arc-gtk.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# This script installs the Arc GTK theme. Meant to be run after install.
+
 function installArcGTKTheme(){
 	echo "Installing Arc GTK theme..."
 	git clone https://aur.archlinux.org/arc-gtk-theme.git ~/arc-gtk-theme
