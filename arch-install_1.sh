4b5ca309c115e1423d265eb6ab94287d84b8e68b
monett010 <acts2018+gh@gmail.com>
2026-04-25 15:28:16 -0400

    Added messages indicating what partitions are being mounted in arch-install_1.sh

--- arch-install_1.sh
+++ arch-install_1.sh
@@ -10,11 +10,14 @@ read -p "What partition does / go? " root
 
 read -p "Would you like a separate /home partition? y/n (default): " answer
 if [ "$answer" == "y" ]; then
-    read -p "Please type the partition you'd like to make your /home partition: " partition
-    mount --mkdir "$partition" /mnt/home
+    read -p "Please type the partition you'd like to make your /home partition: " home_
+    echo "Mounting ${home_} as /home..."
+    mount --mkdir "$home_" /mnt/home
 fi
 
+echo "Mounting ${bootloader_} as /boot/efi..."
 mount "$bootloader_" /mnt/boot/efi
+echo "Mounting ${root} as /..."
 mount "$root" /mnt
 
 # installing linux kernel, headers, firmware, and base packages
